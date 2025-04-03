9f9551b71a33bfcc4991b3327dfabd4059d7d178
Adrian Steinsleger <asteins@gmail.com>
2025-04-02 19:42:36 -0500

    Add vite + React + PWA support

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: 'Musica Universalis',
+        short_name: 'Musica Universalis',
+        description: 'A sound synthesis application based on the movement of celestial bodies',
+        theme_color: '#ffffff',
+        icons: [
+          { src: '/pwa-192x192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/pwa-512x512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      },
+      registerType: 'autoUpdate'
+    })
+  ]
+})
